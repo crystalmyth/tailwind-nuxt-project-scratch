376e8374c01c33aa85b7cb02448470b8e99c2753
crystalmyth <lakshayverma.lv@gmail.com>
2022-04-19 16:03:19 +0530

    sync

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,10 +1,10 @@
-export const IncludeFooter = () => import('../../components/include/Footer.vue' /* webpackChunkName: "components/include-footer" */).then(c => wrapFunctional(c.default || c))
-export const IncludeNavbar = () => import('../../components/include/Navbar.vue' /* webpackChunkName: "components/include-navbar" */).then(c => wrapFunctional(c.default || c))
-export const IncludeSidebar = () => import('../../components/include/Sidebar.vue' /* webpackChunkName: "components/include-sidebar" */).then(c => wrapFunctional(c.default || c))
 export const HomeCTASection = () => import('../../components/home/CTASection.vue' /* webpackChunkName: "components/home-c-t-a-section" */).then(c => wrapFunctional(c.default || c))
 export const HomeFeatureSection = () => import('../../components/home/FeatureSection.vue' /* webpackChunkName: "components/home-feature-section" */).then(c => wrapFunctional(c.default || c))
 export const HomeHeroSection = () => import('../../components/home/HeroSection.vue' /* webpackChunkName: "components/home-hero-section" */).then(c => wrapFunctional(c.default || c))
 export const HomeTestimonialSection = () => import('../../components/home/TestimonialSection.vue' /* webpackChunkName: "components/home-testimonial-section" */).then(c => wrapFunctional(c.default || c))
+export const IncludeFooter = () => import('../../components/include/Footer.vue' /* webpackChunkName: "components/include-footer" */).then(c => wrapFunctional(c.default || c))
+export const IncludeNavbar = () => import('../../components/include/Navbar.vue' /* webpackChunkName: "components/include-navbar" */).then(c => wrapFunctional(c.default || c))
+export const IncludeSidebar = () => import('../../components/include/Sidebar.vue' /* webpackChunkName: "components/include-sidebar" */).then(c => wrapFunctional(c.default || c))
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
--- .nuxt/components/readme.md
+++ .nuxt/components/readme.md
@@ -6,10 +6,10 @@ You can directly use them in pages and other components without the need to impo
 
 **Tip:** If a component is conditionally rendered with `v-if` and is big, it is better to use `Lazy` or `lazy-` prefix to lazy load.
 
-- `<IncludeFooter>` | `<include-footer>` (components/include/Footer.vue)
-- `<IncludeNavbar>` | `<include-navbar>` (components/include/Navbar.vue)
-- `<IncludeSidebar>` | `<include-sidebar>` (components/include/Sidebar.vue)
 - `<HomeCTASection>` | `<home-c-t-a-section>` (components/home/CTASection.vue)
 - `<HomeFeatureSection>` | `<home-feature-section>` (components/home/FeatureSection.vue)
 - `<HomeHeroSection>` | `<home-hero-section>` (components/home/HeroSection.vue)
 - `<HomeTestimonialSection>` | `<home-testimonial-section>` (components/home/TestimonialSection.vue)
+- `<IncludeFooter>` | `<include-footer>` (components/include/Footer.vue)
+- `<IncludeNavbar>` | `<include-navbar>` (components/include/Navbar.vue)
+- `<IncludeSidebar>` | `<include-sidebar>` (components/include/Sidebar.vue)
